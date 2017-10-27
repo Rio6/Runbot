c69a7d52a214c4d75ba872ad039628fabeae6525
Rio6 <rio6.liu@gmail.com>
2017-10-27 16:00:46 -0700

    More operators for Vector

--- src/vector.cpp
+++ src/vector.cpp
@@ -1,40 +1,80 @@
 /*
  * Author: Rio
- * Date: 2017/10/05
+ * Date: 2017/10/27
  */
 
 #include "vector.hpp"
 
 using runbot::Vector;
 
-Vector Vector::operator+(const Vector &rhs) {
+Vector Vector::operator+(const Vector &rhs) const {
     return Vector{x + rhs.x, y + rhs.y};
 }
 
-Vector Vector::operator-(const Vector &rhs) {
+Vector Vector::operator-(const Vector &rhs) const {
     return Vector{x - rhs.x, y - rhs.y};
 }
 
-Vector Vector::operator*(const Vector &rhs) {
+Vector Vector::operator*(const Vector &rhs) const {
     return Vector{x * rhs.x, y * rhs.y};
 }
 
-Vector Vector::operator/(const Vector &rhs) {
+Vector Vector::operator/(const Vector &rhs) const {
     return Vector{x / rhs.x, y / rhs.y};
 }
 
-Vector Vector::operator+(int rhs) {
+Vector Vector::operator+(int rhs) const {
     return Vector{x + rhs, y + rhs};
 }
 
-Vector Vector::operator-(int rhs) {
+Vector Vector::operator-(int rhs) const {
     return Vector{x - rhs, y - rhs};
 }
 
-Vector Vector::operator*(int rhs) {
+Vector Vector::operator*(int rhs) const {
     return Vector{x * rhs, y * rhs};
 }
 
-Vector Vector::operator/(int rhs) {
+Vector Vector::operator/(int rhs) const {
     return Vector{x / rhs, y / rhs};
+}
+
+void Vector::operator+=(const Vector &rhs) {
+    x += rhs.x;
+    y += rhs.y;
+}
+
+void Vector::operator-=(const Vector &rhs) {
+    x -= rhs.x;
+    y -= rhs.y;
+}
+
+void Vector::operator*=(const Vector &rhs) {
+    x *= rhs.x;
+    y *= rhs.y;
+}
+
+void Vector::operator/=(const Vector &rhs) {
+    x /= rhs.x;
+    y /= rhs.y;
+}
+
+void Vector::operator+=(int rhs) {
+    x += rhs;
+    y += rhs;
+}
+
+void Vector::operator-=(int rhs) {
+    x -= rhs;
+    y -= rhs;
+}
+
+void Vector::operator*=(int rhs) {
+    x *= rhs;
+    y *= rhs;
+}
+
+void Vector::operator/=(int rhs) {
+    x /= rhs;
+    y /= rhs;
 }
