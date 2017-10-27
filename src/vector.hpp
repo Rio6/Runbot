c9c198bd54096aa3541ac92457789f50682bef5d
Rio6 <rio6.liu@gmail.com>
2017-10-27 16:00:46 -0700

    More operators for Vector

--- src/vector.hpp
+++ src/vector.hpp
@@ -1,6 +1,6 @@
 /*
  * Author: Rio
- * Date: 2017/10/05
+ * Date: 2017/10/27
  */
 
 #ifndef _VECTOR_H_
@@ -10,14 +10,25 @@ namespace runbot {
     struct Vector {
         int x = 0, y = 0;
 
-        Vector operator+(const Vector&);
-        Vector operator-(const Vector&);
-        Vector operator*(const Vector&);
-        Vector operator/(const Vector&);
-        Vector operator+(int);
-        Vector operator-(int);
-        Vector operator*(int);
-        Vector operator/(int);
+        Vector operator+(const Vector&) const;
+        Vector operator-(const Vector&) const;
+        Vector operator*(const Vector&) const;
+        Vector operator/(const Vector&) const;
+
+        Vector operator+(int) const;
+        Vector operator-(int) const;
+        Vector operator*(int) const;
+        Vector operator/(int) const;
+
+        void operator+=(const Vector&);
+        void operator-=(const Vector&);
+        void operator*=(const Vector&);
+        void operator/=(const Vector&);
+
+        void operator+=(int);
+        void operator-=(int);
+        void operator*=(int);
+        void operator/=(int);
     };
 };
 
