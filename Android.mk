7907ff289fd1cf3b83591a93d9beca5ab168db74
Rio6 <rio6.liu@gmail.com>
2018-02-23 14:20:51 -0800

    Use wildcard in Android.mk

--- Android.mk
+++ Android.mk
@@ -10,7 +10,8 @@ SDL_IMAGE_PATH := ../SDL2_image
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/$(SDL_IMAGE_PATH)
 
 # Add your application source files here...
-LOCAL_SRC_FILES :=  src/anim.cpp src/background.cpp src/bullet.cpp src/collision.cpp src/game.cpp src/graphic.cpp src/level.cpp src/main.cpp src/missile.cpp src/object.cpp src/robot.cpp src/tile.cpp
+LOCAL_SRC_FILES :=  $(wildcard $(LOCAL_PATH)/src/*.cpp)
+LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
 
 LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image
 
