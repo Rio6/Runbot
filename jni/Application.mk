c940275f8e34e8d64633ee63c1f940187a400781
Rio6 <rio6.liu@gmail.com>
2018-04-02 16:37:15 -0700

    Use ndk c++_shared stl lib

--- jni/Application.mk
+++ jni/Application.mk
@@ -2,8 +2,8 @@
 # Uncomment this if you're using STL in your project
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 #APP_STL := stlport_static 
-APP_STL := gnustl_static
-APP_CPPFLAGS += -std=gnu++14 -fexceptions
+APP_STL := c++_shared
+APP_CPPFLAGS += -std=gnu++1z -fexceptions
 
 APP_ABI := armeabi armeabi-v7a x86
 
