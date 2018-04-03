570d4a5264ee9e58d4b504e1e9c830842136f356
Rio6 <rio6.liu@gmail.com>
2018-04-02 18:13:48 -0700

    Add SDL2_mixer include

--- Android.mk
+++ Android.mk
@@ -6,14 +6,15 @@ LOCAL_MODULE := main
 
 SDL_PATH := ../SDL2
 SDL_IMAGE_PATH := ../SDL2_image
+SDL_MIXER_PATH := ../SDL2_mixer
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/$(SDL_IMAGE_PATH) $(LOCAL_PATH)/include
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/$(SDL_PATH)/include $(LOCAL_PATH)/$(SDL_IMAGE_PATH) $(LOCAL_PATH)/$(SDL_MIXER_PATH) $(LOCAL_PATH)/include
 
 # Add your application source files here...
 LOCAL_SRC_FILES :=  $(wildcard $(LOCAL_PATH)/src/*.cpp)
 LOCAL_SRC_FILES := $(LOCAL_SRC_FILES:$(LOCAL_PATH)/%=%)
 
-LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image
+LOCAL_SHARED_LIBRARIES := SDL2 SDL2_image SDL2_mixer
 
 LOCAL_LDLIBS := -lGLESv1_CM -lGLESv2 -llog -latomic
 
