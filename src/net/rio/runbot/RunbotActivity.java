9f3c24763d06bd09d3de61346ed7feda15292e9d
Rio6 <rio6.liu@gmail.com>
2018-05-09 16:37:03 -0700

    Hide navigation bar

--- src/net/rio/runbot/RunbotActivity.java
+++ src/net/rio/runbot/RunbotActivity.java
@@ -1,3 +1,35 @@
 package net.rio.runbot;
+
+import android.os.Build;
+import android.view.View;
 import org.libsdl.app.SDLActivity;
-public class RunbotActivity extends SDLActivity {}
+
+public class RunbotActivity extends SDLActivity {
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            final View decorView = getWindow().getDecorView();
+            final int uiOptions =
+                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                View.SYSTEM_UI_FLAG_FULLSCREEN |
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+            if(Build.VERSION.SDK_INT >= 11) {
+                decorView.setSystemUiVisibility(uiOptions);
+                decorView.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+                    @Override
+                    public void onSystemUiVisibilityChange(int visibility) {
+                        if ((visibility & View.SYSTEM_UI_FLAG_FULLSCREEN) == 0) {
+                            decorView.setSystemUiVisibility(uiOptions);
+                        }
+                    }
+                });
+            }
+        }
+    }
+}
