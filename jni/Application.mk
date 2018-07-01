fd0ceabf19fe66a6432af80eb847301ccb8449f4
Rio6 <rio6.liu@gmail.com>
2018-07-01 15:54:53 -0700

    Suppress annoying warning

--- jni/Application.mk
+++ jni/Application.mk
@@ -3,7 +3,7 @@
 # See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
 #APP_STL := stlport_static 
 APP_STL := c++_shared
-APP_CPPFLAGS += -std=gnu++14 -fexceptions -frtti
+APP_CPPFLAGS += -std=gnu++14 -fexceptions -frtti -Wno-parentheses
 
 APP_ABI := armeabi armeabi-v7a x86
 
